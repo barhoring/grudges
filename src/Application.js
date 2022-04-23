52805c65ddada675845a367c0c459490070ad09b
Bar Horing Amir <bar.apple@gmail.com>
2022-04-23 22:21:40 +0300

    useReducer with local storage

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 import id from 'uuid/v4';
 
@@ -7,22 +7,58 @@ import NewGrudge from './NewGrudge';
 
 import initialState from './initialState';
 
-const Application = () => {
-  const [grudges, setGrudges] = useState(initialState);
+const ADD_GRUDGE = 'ADD_GRUDGE';
+const REMOVE_GRUDGE = 'REMOVE_GRUDGE';
 
-  const addGrudge = grudge => {
-    grudge.id = id();
-    grudge.forgiven = false;
-    setGrudges([grudge, ...grudges]);
+const reducer = (state, action) => {
+  if (action.type === ADD_GRUDGE) {
+    return [{ ...action.payload }, ...state];
+  }
+  if (action.type === REMOVE_GRUDGE) {
+    return state.map((grudge) => {
+      if (grudge.id !== action.payload.id) return grudge;
+      return { ...grudge, forgiven: !grudge.forgiven };
+    });
+  }
+  return state;
+};
+
+const useLocalStorage = (initialState, key) => {
+  const get = () => {
+    let storage = localStorage.getItem(key);
+    storage = JSON.parse(storage);
+    debugger;
+    if (storage && Object.keys(storage).length > 0) {
+      return storage[key];
+    }
+    console.log('here2');
+    return initialState;
   };
 
-  const toggleForgiveness = id => {
-    setGrudges(
-      grudges.map(grudge => {
-        if (grudge.id !== id) return grudge;
-        return { ...grudge, forgiven: !grudge.forgiven };
-      })
-    );
+  const [grudges, dispatch] = useReducer(reducer, get());
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify({ [key]: grudges }));
+  }, [grudges]);
+
+  return [grudges, dispatch];
+};
+
+const Application = () => {
+  const [grudges, dispatch] = useLocalStorage(initialState, 'my_grudges');
+
+  const addGrudge = ({ person, reason }) => {
+    dispatch({
+      type: ADD_GRUDGE,
+      payload: { person, reason, id: id(), forgiven: false }
+    });
+  };
+
+  const toggleForgiveness = (id) => {
+    dispatch({
+      type: REMOVE_GRUDGE,
+      payload: { id }
+    });
   };
 
   return (
